90fd986430830740caa77c2f22d56cdee06f3acd
zym0520 <1274515642@qq.com>
2018-11-15 17:09:54 +0800

    第二次作业

--- Prime.c
+++ Prime.c
@@ -14,17 +14,19 @@ int pleacon(int x)
  } 
  int main(void)
  {
- 	int i = 100;
- 	while(i <= 999)
+ 	int i = 100,product;
+ 	while(i <= 9999)
  	{
- 		int a = i / 100;
- 		int b = i / 10 % 10;
- 		int c = i % 10;
- 		int sum1 = a + b + c;
- 		int sum2 = a*a + b*b + c*c;
- 		int product = a*b*c;
+ 		int a = i / 1000;
+ 		int b = i / 100 % 10;
+ 		int c = i / 10 % 10;
+ 		int d = i % 10;
+ 		int sum1 = a + b + c +d;
+ 		int sum2 = a*a + b*b + c*c + d*d;
+ 		if (a == 0) product = b * c *d;
+ 		else product = a * b * c *d;
  		if(pleacon(i) == 0 && pleacon(sum1) == 0 && pleacon(sum2) == 0 && pleacon(product) == 0)
- 		printf("%d ",i);
+		 printf("%d ",i);
  		i++;
 	 }
 	 return 0;
